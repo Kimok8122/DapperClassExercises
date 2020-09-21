a9faff466c0a46497b7d59ba1b22e1b1ebe6148b
Mona Lisa <email@example.com>
2020-09-21 12:47:48 -0600

    ignored appsetting

---
+++ DrapperClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DrapperClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
